7982007a4a1a7872c67f3e1ba36a0287e1c1b560
Galih Arghubi_208829 <galih.arghubi@bfi.co.id>
2025-08-15 17:41:09 +0700

    revert: try with theo payload

--- triggers/underwriting/ca.js
+++ triggers/underwriting/ca.js
@@ -24,46 +24,32 @@ const generateLicensePlate = () => {
 const licensePlate = generateLicensePlate();
 // const licensePlate = "LZ5835AZZ";
 const payload = {
-  "$.status.application": "approved",
-  "$.asset.license_plate": `${licensePlate}`,
+  "$.status.application": "pre_approved",
+  "$.asset.license_plate": licensePlate,
   "$.customer.ktp.nik": "3173334212960003",
   "$.customer.ktp.name": `${cleansedName(
-    faker.person.firstName()
-  )} ${cleansedName(faker.person.lastName())}`,
+    faker.person.firstName
+  )} ${cleansedName(faker.person.lastName)}`,
   "$.loan_structure.risk_level": "HIGH",
   "$.loan_structure.product_id": 1,
-  "$.process.survey_task.survey_branch_id": "401",
+  "$.process.survey_task.survey_branch_id": "402",
   "$.process.survey_task.surveyor_employee_id": "000004",
-  "$.process.operations_task.processing_branch_id": "401",
+  "$.process.operations_task.processing_branch_id": "402",
   "$.submission_date": "2025-04-07T18:00:00Z",
   "$.customer.contact.mobile_number": "+6281234567891",
-  "$.customer.ktp.birth_date": "1980-12-03",
+  "$.customer.ktp.birth_date": "1974-12-03",
   "$.customer.ktp.birth_place": "jakarta",
   "$.customer.ktp.gender": "F",
   "$.customer.professional.npwp": "123123123123123",
   "$.customer.personal.marital_status_code": "M",
   "$.spouse.ktp.nik": "3173334212960003",
-  "$.spouse.ktp.name": `${cleansedName(
-    faker.person.firstName()
-  )} ${cleansedName(faker.person.lastName())}`,
+  "$.spouse.ktp.name": `${cleansedName(faker.person.firstName)} ${cleansedName(
+    faker.person.lastName
+  )}`,
   "$.spouse.mobile_number": "+6281234567890",
   "$.customer.personal.number_dependents": 2,
   "$.documents.ktp.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
   "$.documents.kk.document_id": "dc9991b3-a826-481d-bc19-1bdccf555f75",
-  "$.documents.bkr_house_rent_ownership.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.asset_payment_receipt.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.asset_payment_invoice.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.asset_payment_agreement.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.release_letter.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.asset.in_front_of_bfi_office.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.asset_dump_truck_on_lift_position.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.asset_photo_bpkb_productive.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.business_legality.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.proof_photo_consumer_still_working.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.active_spk.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.proof_of_payment_for_other_contracts_that_are_still_in_arrears.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.signing_by_waarmerking_or_legalization.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
-  "$.documents.cross_collateral_and_default_all_active_contracts.document_id": "9888d558-8480-4dbe-95fd-07e2dadbede3",
   "$.documents.selfie.document_id": "17d25972-e99f-4ec0-91d9-540a7090cdb2",
   "$.documents.spouse_ktp.document_id": "2e564529-d5b4-407b-adfe-a725d903bb3b",
   "$.documents.interview.document_id": "4f7a5a7e-32f7-46a9-8c4e-f409bd7e0163",
@@ -82,81 +68,56 @@ const payload = {
   "$.loan_structure.purpose.finance_purpose_details":
     "Detail Kendaraan untuk Usaha",
   "$.loan_structure.purpose_of_financing": "PRODUCTIVE_LONG",
-  "$.process.returning.customer_type": "RO_EXP",
+  "$.process.returning.customer_type": "RO_ACTIVE",
   "$.loan_structure.monthly_installment": 2900000,
-  "$.loan_structure.provisional_amount": 10000000,
+  "$.loan_structure.provisional_amount": 1000000,
   "$.customer.domicile.ownership_code": "KL",
-  "$.customer.domicile.address.street_address": "Ini alamat yaaaa 192810",
-  "$.customer.domicile.stay_since": 2023,
+  "$.customer.domicile.address.street_address": "816 Bergstrom Valley",
+  "$.customer.domicile.stay_since": "2023",
   "$.customer.domicile.bkr_form": "RK_LISTRIK",
   "$.customer.domicile.name_on_bkr": "SUPARMAN",
   "$.documents.house_ownership.document_id":
     "c507e104-36c1-49d6-8d7d-4931fee86559",
   "$.documents.house.document_id": "5142ab92-82b1-49b1-a4dc-4d175ba2a75f",
-  "$.asset.asset_code": "MITSUBISHI.XPANDER.CROSS15MT",
-  "$.asset.manufacturing_year": 2024,
+  "$.asset.asset_code": "DAIHATSU.AYLA.D10AT",
+  "$.asset.manufacturing_year": 2021,
   "$.asset.asset_usage": "COMMERCIAL",
   "$.process.asset_pricing.price": 150000000,
   "$.loan_structure.ltv": 0.7929,
   "$.loan_structure.tenure": 6,
   "$.loan_structure.interest_rate": 0.0312,
-  "$.loan_structure.ntf_amount": 198000000,
   "$.loan_structure.product_offering": 1,
+
   "$.asset.bpkb_ownership": "1",
   "$.asset.bpkb_owner_name": "Adhitya",
   "$.documents.asset.asset_front.document_id":
     "ed668a54-93ec-4f36-a888-f84007baf190",
   "$.documents.asset.asset_rear.document_id":
     "23828c5e-7c35-4ef1-aea6-72b9305deda7",
-  "$.documents.asset.asset_front_left_side.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.asset_rear_right_side.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.asset_rear_left_side.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.asset_front_right_side.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.asset_interior_center_sid.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.asset_left_side.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.asset_right_side.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.engine_number.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.selfie_with_vehicle.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.asset.speedometer.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.bpkb_receipt_2.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.customer_receipt_2.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.payment_receipt.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.bpkb_page_1.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.bpkb_page_2.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.bpkb_page_3.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.bpkb_page_4.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.bpkb_page_5.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.vehicle_inspection.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.invoice.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.ktp_bpkb.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.chassis_number.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.stnk.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.tax_notice.document_id": "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.release_letter.document_id":
-    "ed668a54-93ec-4f36-a888-f84007baf190",
-  "$.documents.income_proof.document_id":
-    "31156555-2489-4708-bf19-e8f57e4fb390",
-  "$.documents.business_photo.document_id":
-    "31156555-2489-4708-bf19-e8f57e4fb390",
-  "$.documents.business_location.document_id":
-    "31156555-2489-4708-bf19-e8f57e4fb390",
+
   "$.documents.business_legality.document_id":
-    "31156555-2489-4708-bf19-e8f57e4fb390",
+    "2a2094a0-c5ad-473f-bbe7-4ad8d531ba2f",
+  "$.documents.income_proof.document_id":
+    "74618387-68f3-44fd-9e46-c04d646f15e5",
+  "$.documents.business_photo.document_id":
+    "96a6b314-fa8b-45b2-87ab-45cba34421c4",
+  "$.documents.business_location.document_id":
+    "01ed0371-12f2-4e80-822a-49096ac3ae6d",
+  "$.customer.professional.occupation_type_code": "M",
+  "$.customer.professional.occupation_code": "BURARTASRT",
+  "$.customer.professional.company_name": "Boyer - Lehner",
+  "$.customer.professional.company_address": "jln mangkubumi",
+  "$.customer.professional.company_phone": "+628127819287",
+  "$.customer.professional.economic_sector": "01",
+  "$.customer.professional.industry": "151110-1",
+  "$.customer.professional.employment_status": "PERMANENT_EMPLOYEE",
+  "$.customer.professional.business_since_year": 2020,
+  "$.customer.emergency_contact.name": "Anne Bartoletti",
+  "$.customer.emergency_contact.relation_with_customer": "FAMILY",
+  "$.customer.emergency_contact.street_address": "Jln Mayang sari IV no 12",
+  "$.customer.emergency_contact.mobile_number": "+6281202948172",
+  "$.customer.domicile.bkr_period": "2023-01-01",
+
   "$.process.credit_checking.neighborhood.source_1.information_source":
     "NEIGHBOR",
   "$.process.credit_checking.neighborhood.source_1.informant_name": "Kim Jisoo",
@@ -171,33 +132,34 @@ const payload = {
   "$.process.credit_checking.neighborhood.source_1.unit_seen_at_residence_more_than_once": true,
   "$.process.credit_checking.neighborhood.source_1.consumer_often_at_home": true,
   "$.process.credit_checking.neighborhood.source_1.consumer_affiliate_with_organization": false,
+
   "$.process.credit_checking.neighborhood.source_1.house_suitability": true,
   "$.asset.bpkb_ownership_period": "FROM_1_TO_6_MONTH",
   "$.process.credit_checking.neighborhood.source_1.other_information":
     "Jualan bakso di siang hari, jualan ketoprak di malam hari.",
-  "$.customer.emergency_contact.name": "Jennie Kim",
-  "$.customer.emergency_contact.relation_with_customer": "FAMILY",
-  "$.customer.emergency_contact.street_address":
-    "Jalan Pahlawan Seratus No. 1000, Serpong",
-  "$.customer.emergency_contact.mobile_number": "+6281234567890",
-  "$.branch.branch_id": "401",
-  "$.process.survey_task.survey_branch_name": "MEDAN",
-  "$.channel.partner_internal_name": "partner-goto",
-  "$.channel.soa_id": "28",
-  "$.channel.partner_id": "907d075e-5cef-4179-940e-794946b6eb33",
-  "$.customer.domicile.address.sub_district_code": "15.07.02.2008",
 
-  "$.loan_structure.purpose_of_financing": "PRODUCTIVE_LONG",
-  "$.customer.professional.occupation_type_code": "M",
-  "$.customer.professional.occupation_code": "BURARTASRT",
-  "$.customer.professional.company_name": "Boyer - Lehner",
-  "$.customer.professional.company_address": "jln mangkubumi",
-  "$.customer.professional.company_phone": "+628127819287",
-  "$.customer.professional.economic_sector": "01",
-  "$.customer.professional.industry": "151110-1",
-  "$.customer.professional.employment_status": "PERMANENT_EMPLOYEE",
-  "$.customer.professional.business_since_year": 2020,
-  "$.customer.domicile.bkr_period": "2023-01-01",
+  "$.documents.asset_document_status": "IN_TRANSIT_CUSTOMER_TO_BRANCH",
+
+  "$.asset.bpkb_address": "Perum Taman Bojongsari No.58",
+  "$.asset.bpkb_invoice_number": "19069692004JK9012027",
+  "$.asset.bpkb_number": "P234567890",
+  "$.asset.unit_color": "ABU ABU",
+  "$.asset.chassis_number": "P1234567890129831",
+  "$.asset.engine_number": "L15Z15605255",
+  "$.documents.bpkb_page_1.document_id": "b6bee008-eb52-406b-be14-54d041025bb8",
+  "$.documents.bpkb_page_2.document_id": "b6bee008-eb52-406b-be14-54d041025bb8",
+  "$.documents.bpkb_page_3.document_id": "b6bee008-eb52-406b-be14-54d041025bb8",
+  "$.documents.bpkb_page_4.document_id": "b6bee008-eb52-406b-be14-54d041025bb8",
+
+  "$.documents.bpkb_invoice.document_id":
+    "b6bee008-eb52-406b-be14-54d041025bb8",
+
+  "$.channel.soa_id": "18",
+  "$.channel.partner_id": "b6bee008-eb52-406b-be14-54d041025bb8",
+  "$.channel.partner_internal_name": "partner-name-lower-kebab-case",
+  "$.branch.branch_id": "412",
+  "$.documents.asset.selfie_with_vehicle.document_id":
+    "b6bee008-eb52-406b-be14-54d041025bb8",
 };
 
 export const Ca = async () => {
