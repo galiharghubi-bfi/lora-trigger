eed25955b0c65923e73a09454f21f5b191de6d16
Galih Arghubi_208829 <galih.arghubi@bfi.co.id>
2025-12-28 17:13:07 +0700

    feat: fix page info lainnya and add new user

--- triggers/automation/pages/ndf4w-high/page-05-info-lainnya.js
+++ triggers/automation/pages/ndf4w-high/page-05-info-lainnya.js
@@ -5,27 +5,25 @@
 export const Page5InfoLainnya = (taskId, params = {}) => ({
   formName: "form_personal_information_show",
   payload: {
-    "$.customer.personal.mother_maiden_name": "Siti Nuraedah",
+    "$.customer.personal.mother_maiden_name": "SITI NURAEDAH",
     "$.customer.contact.email": "LULLABYANGEL22@GMAIL.COM",
     "$.customer.professional.npwp_type": "NPWP",
     "$.customer.domicile.name_on_bkr": "SUPARMAN",
     "$.customer.domicile.bkr_form": "RK_LISTRIK",
     "$.customer.domicile.is_name_bkr_equal": "true",
-    "ltw.custom_customer_professional_economic_sector": "JASA",
-    "ltw.custom_customer_professional_industry":
-      "ACCESSORIES & OTHER (ACRYLIC, GANTUNGAN KUNCI, STICKER & SEJENISNYA)",
+    "$.customer.professional.economic_sector": "07",
+    "$.customer.professional.industry": "012210-1",
     "$.customer.professional.company_sub_district_code": "31.74.07.1001",
     "$.customer.emergency_contact.name": "JONATHAN",
     "$.customer.emergency_contact.relation_with_customer": "FAMILY",
     "$.customer.emergency_contact.mobile_number": "+62851282888",
-    "$.customer.emergency_contact.street_address": "Jl. Swdada 24",
+    "$.customer.emergency_contact.street_address": "JL. SWDADA 24",
     "$.customer.emergency_contact.sub_district_code": "61.01.16.2005",
     "$.customer.emergency_contact.rt": "012",
     "$.customer.emergency_contact.rw": "001",
     "$.documents.ktp.document_id": "9c93b777-1a00-4def-9643-60cff93b75e4",
-    "$.documents.employment_evidence.document_id":
-      "ec928907-c1cf-4d88-8258-bb0530c3c48e",
-    "$.documents.npwp.document_id": "a9c35f5c-bef1-4133-8b1f-3ffef5b0bf12",
+    "$.documents.employment_evidence.document_id": "",
+    "$.documents.npwp.document_id": "51b0b10b-1494-46d3-9bad-a0b1b6a7d61e",
     action: "submit",
   },
 });
