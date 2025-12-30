c4dd0fb65cb12ea26cca42934266d90cd1a4eb0a
ptdede <putu.dede@gmail.com>
2025-12-30 16:00:10 +0700

    feat: bump document to 0.8.0

--- triggers/start-application.js
+++ triggers/start-application.js
@@ -5,7 +5,7 @@ export function StartApplication() {
 
   const payload = {
     id: workflowId,
-    name: "dp-ndf-v0_7_0",
+    name: "dp-ndf-v0_8_0",
   };
   const start = fetch(`${baseConfig.lgs_base_url}/application/start`, {
     method: "POST",
